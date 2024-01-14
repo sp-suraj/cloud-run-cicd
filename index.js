c049bf62ff972dc868206a1956f88dfe2c59fc3d
sp-suraj <surajpraja124@gmail.com>
2024-01-14 12:27:12 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.status(200).json([
+    {
+      id: 1,
+      name: "Suraj",
+    },
+    {
+      id: 2,
+      name: "Sukhi",
+    },
+    {
+      id: 3,
+      name: "Suman",
+    },
+    {
+      id: 4,
+      name: "Ruchitha",
+    },
+  ]);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on server 4000");
+});
